d543b7759dd210748afc592acfe380586dd2c655
Jesse Smith <jesse.lee.smith@gmail.com>
2021-09-09 16:09:13 -0700

    commit tree 6d892d263f472ddec3397f7b4d9785ab8bc9e726

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,8 @@
 # code your solution here
+def power_digit_sum(base, exponent)
+    num = base ** exponent
+
+    num.to_s.split('').reduce do |sum, digit|
+        sum.to_i + digit.to_i
+    end
+end
